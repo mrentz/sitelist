029401eb84f0711c8ee567c0b359c4bae76ba975
Matthew <mrentz@mail.com>
2017-09-18 00:37:05 +0930

    new site form working

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -9,10 +9,30 @@ class SitesController < ApplicationController
     @sites = Site.all
   end
 
-  def siteinfo
+  def show
     @site = Site.find(params[:id])
   end
 
   def new
+    @site = Site.new
   end
+
+  def create
+    site = Site.new(post_params)
+
+    if site.save
+      redirect_to site_path(site)
+    else
+      redirect_to new_site_path
+    end
+
+
+  end
+
+
+  private
+  def post_params
+    params.require(:site).permit(:site, :date, :description, :url)
+  end
+  
 end
