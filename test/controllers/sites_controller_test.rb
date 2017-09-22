d00be39607b378ad35fc52d928ba9a0eaf1c2a32
Matthew <mrentz@mail.com>
2017-09-22 17:16:31 +0930

    starting with automated testing

--- test/controllers/sites_controller_test.rb
+++ test/controllers/sites_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class SitesControllerTest < ActionDispatch::IntegrationTest
-  test "should get sitelist" do
+
+    test "should get sitelist" do
     get sites_path
     assert_response :success
+    assert_select "h1", "List of Websites"
   end
+
+    test "should get new website form page" do
+    get new_site_path
+    assert_response :success
+    assert_select "h1", "Save Your Favourite Website"
+    end
 
 end
--- test/models/site_test.rb
+++ test/models/site_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class SiteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  
+  def setup
+    @site = Site.new(site: "Example Site", date: Date.today,
+                     description: "Website description text", url: "site_url")
+  end
+
+  test "should be valid" do
+    assert @site.valid?
+  end
+
+    test "name should be present" do
+    @site.site = "     "
+    assert_not @site.valid?
+  end
+
+    test "description should be present" do
+    @site.description = "     "
+    assert_not @site.valid?
+  end
+
+    test "url should be present" do
+    @site.url = "     "
+    assert_not @site.valid?
+  end
+   
+  test "site name should be unique" do
+    duplicate_site = @site.dup
+    duplicate_site.site = @site.site#.upcase
+    @site.save
+    assert_not duplicate_site.valid?
+  end
+   
+  test "Description should be unique" do
+    duplicate_site = @site.dup
+    duplicate_site.description = @site.description#.upcase
+    @site.save
+    assert_not duplicate_site.valid?
+  end
+   
+  test "URL should be unique" do
+    duplicate_site = @site.dup
+    duplicate_site.url = @site.url#.upcase
+    @site.save
+    assert_not duplicate_site.valid?
+  end
+  
+
 end
